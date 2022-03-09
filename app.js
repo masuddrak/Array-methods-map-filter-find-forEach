5dd2e801ac7cdf16098e686349f8c5519f04211a
masud rana <masudranaacb@gmail.com>
2022-03-09 12:31:06 -0800

    array map method using

---
+++ app.js
@@ -0,0 +1,13 @@
+const products=[
+    {name:'phone', brand:'iphone',price:50000,color:'silver'},
+    {name:'watch', brand:'casio',price:500,color:'white'},
+    {name:'laptop', brand:'acer',price:53000,color:'golden'},
+    {name:'phone', brand:'lg',price:5000,color:'silver'},
+    {name:'phone', brand:'htc',price:45000,color:'yello'}
+]
+// map using array of object
+const brand=products.map(product=>product.color)
+console.log(brand)
+
+const price=products.map(product=>product.price)
+console.log(price)
